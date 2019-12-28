4c8b89f9f0b832b7f2058d761e84ec30b64f4322
Jeshua Hicks <jhicks7884@outlook.com>
2019-12-28 14:03:41 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,12 @@
 class Pokemon
+  attr_accessor :name, :type, :id, :db
+
+
+  def initialize(keywords)
+
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
 end
